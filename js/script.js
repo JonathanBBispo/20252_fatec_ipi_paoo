b9d7ea4930cb5bb96680b491824abca005898d49
Jonathan Bispo <jokz.bis@gmail.com>
2025-08-06 18:12:15 -0300

    learn(ap01-js): encerra o codigo 1.4.1

---
+++ js/script.js
@@ -0,0 +1,62 @@
+//comparação
+// console.log([] == [])
+// console.log([1] == [1])
+// console.log(0 == [0])
+// while(true) console.log( true == true)SS
+// console.log( true == '1')
+// console.log( true == 1)
+//operadores == e ===
+// console.log('1' === 1)
+// console.log(1 == '1')
+// console.log( 1 == 1)
+
+//coerção
+// const n1 = 2
+// const n2 = '3'
+//coerção implícita
+// const n3 = n1 + n2
+// console.log(n3)
+//coerção implícita
+// const n4 = n1 + Number(n2)
+// console.log(n4)
+
+//sistema de tipos
+// let idade = 20
+// console.log(typeof(idade))
+// const nome = "Ana"
+// console.log(typeof(nome))
+// idade = 'vinte'
+// console.log(typeof(idade))
+
+//declaração de variáveis(const, let e var)
+//const
+// const nome = 'José'
+// console.log(nome)
+// nome = 'José da Silva' //por se tratar de um const, a variável não pode ser alterada, dando erro de compilação
+
+//let
+// let a = 2
+// let nome = 'José'
+// console.log(a)
+// console.log(nome)
+// nome = 'José da silva' //a variável pode ser alterada
+// console.log(nome)
+
+//var
+// var c = 2
+// var nome = 'Jose'
+// nome = "José da Silva"
+// console.log(nome)
+// var linguagem = 'Javascript'
+// console.log('Aprendendo ' + linguagem)
+// var linguagem = "Java"
+// console.log('Aprendendo ' + linguagem)
+// console.log("Exemplo contra intuitivo usando var")
+//host içar
+// var idade = 18
+// console.log("Oi, " + nome)
+// if(idade >= 18){
+//     var nome = "João"
+//     console.log("Sim, " + nome + ". Você pode dirigir.")
+// }
+// console.log(`Até mais, ${nome}`)
